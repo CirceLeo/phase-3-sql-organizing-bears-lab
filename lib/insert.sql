63fcca014287ba07137278675ce13798be7d5b82
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-07 10:37:27 -0400

    commit tree 33182421077a2936aef1254c9facb09631aa4804

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Mr. Chocolate', 3, 'F', 'pink', 'quiet', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Rowdy', 5, 'M', 'pink', 'loud', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Tabitha', 1, 'F', 'red', 'sad', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Sergeant Brown', 8, 'F', 'blue', 'rowdy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Melissa', 5, 'F', 'magenta', 'silly', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Grinch', 6, 'F', 'green', 'evil', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+'Wendy', 1, 'M', 'pink', 'odd', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (
+null, 5, 'M', 'pink', 'loud', 1);
